da5c3b96267b4ae1393e0123e8cf0af9a6906dcb
vivekpkadam220482 <vivek.p.kadam@gmail.com>
2025-08-29 21:55:43 +0530

    Refactor login test to use variable locators for improved readability

--- tests/Test1.spec.js
+++ tests/Test1.spec.js
@@ -1,15 +1,22 @@
 import { test, expect } from '@playwright/test';
 
 test.only('Login test', async ({ page }) => {
+  const username = page.locator('#username');
+  const password = page.locator('#password');
+  const signIn = page.locator('#signInBtn');
+ 
   await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
-  await page.locator('#username').fill('rahulshettyaasacacademy');
-  await page.locator('#password').fill('learning');
-  await page.locator('#signInBtn').click();
-  // console.log(await page.locator("[style*='block']").textContent());
-await expect(page.locator("[style*='block']")).toContainText('Incorrect');
-await page.locator('#username').fill('');
-await page.locator('#username').fill('rahulshettyacademy');
-await page.locator('#signInBtn').click();
+  await username.fill('rahulshettyacadsdcscdssemy');
+  await password.fill('learning2');
+  await signIn.click();
+  await expect(page.locator("[style*='block']")).toContainText('Incorrect');
+  
+  await username.fill('');
+  await password.fill('');
+  await username.fill('rahulshettyacademy');
+  await password.fill('learning');
+  await signIn.click();
+
 let outPut = await page.locator(".card-title a").first().textContent();
 if (outPut == "iphone X") console.log("Test case passed");
 });
